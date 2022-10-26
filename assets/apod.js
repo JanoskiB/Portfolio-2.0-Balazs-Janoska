aae41d0803aafd5875eb20868cdb02fa67d97005
Balazs Janoska <blage97@gmail.com>
2022-10-27 00:45:34 +0200

    Draft1.0

---
+++ assets/apod.js
@@ -0,0 +1,39 @@
+
+let NASAkey = "imQvbpDRVnFzNAwvbr0V8qSMVH10p8S0dNtLsUYQ";   
+
+let NASAurl = "https://api.nasa.gov/planetary/apod";        
+
+
+
+getDataNASA();                                              
+
+ 
+function getDataNASA() {                                   
+  const xhttp = new XMLHttpRequest();                       
+  xhttp.onreadystatechange = function () {                  
+    if (this.readyState == 4 && this.status == 200) {       
+      const data = JSON.parse(this.responseText);           
+      renderNASA(data);                                     
+      console.log(data);                                    
+    }
+  }
+  xhttp.open("GET", `${NASAurl}?api_key=${NASAkey}`, true);  
+  xhttp.send();                                              
+}
+
+function renderNASA(data){                                   
+  document.querySelector('.apod-wrapper').innerHTML =`              
+  <h5>Astronomy picture of the day:<h5>
+  <h1>${data.title}</h1>
+  <img src="${data.url}" alt="APOD"> <br>
+  <iframe width="800px" height="600px"
+src="${data.url}">
+</iframe>
+  <p>${data.date}<p>
+  <p class="apod-explanation">${data.explanation}</p>
+  <p>Copyright: ${data.copyright}</p>
+  `
+}
+
+
+  
